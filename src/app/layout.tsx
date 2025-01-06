fad043c1caa4039ab1089beb07d5c4dd518c7f42
Seb <jonesseb10@gmail.com>
2025-01-06 14:25:28 +0000

    Fix v2 - preventing zoom in input use

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -26,7 +26,10 @@ export default function RootLayout({
   return (
     <html lang="en">
       <head>
-        <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no" />
+        <meta 
+          name="viewport" 
+          content="width=device-width, initial-scale=1, maximum-scale=1, minimum-scale=1, user-scalable=0, viewport-fit=cover" 
+        />
       </head>
       <body className={inter.className}>
         <Providers>
